ffad89332c5e90d55034e1a65e03b8713c3b5bcb
Valery Pobelenskiy <valery_po85@mail.ru>
2021-02-05 00:56:10 +0600

    First commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
 
-const menuBtn = document.querySelector(".m-menu");
-const menu = document.querySelector(".menu-sidebar");
-
-
-
-menuBtn.addEventListener("click", function() {
-	
-	menu.classList.toggle("active");
-	
-});
