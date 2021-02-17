2733f6b1c5c84c7221d8fd7d5752932d98df2d28
Valery Pobelenskiy <valery_po85@mail.ru>
2021-02-17 19:58:08 +0600

    Miha

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
+ let btnPlus = document.querySelector(".contact-form__button--plus");
+ let  formWrapper = document.querySelector(".form-row__wrapper");
+
+
+
+btnPlus.addEventListener("click", function () {
+  
+  const taskTHML = `<div class="form-row__bottom">
+              <label class="contact-form__label">
+               <input type="text" class="contact__input" placeholder="172.16.45.75">
+              </label>
+              <label class="contact-form__label">
+              <input type="text" class="contact__input" placeholder="/16">
+              </label>
+              <label class="contact-form__label">
+              <input type="text" class="contact__input" placeholder="Внутренняя сеть АС">
+              </label>
+            
+             </div>`;
+  
+ formWrapper.insertAdjacentHTML("beforeEnd", taskTHML);
+   console.log(taskTHML);
+  
+})
+
+
+document.querySelector(".contact-form__button--minus").addEventListener("click", (e) => {
+  
+  document.querySelector(".form-row__bottom").remove();
+})
+
+  
+  
+  
+  
+
+
+
+  
+
+     
+  
+ 
+  
+
 
